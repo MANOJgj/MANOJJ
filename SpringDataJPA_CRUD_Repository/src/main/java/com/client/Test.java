49490b3701c084438e899dc4c831ea9822c37bc6
MANOJgj <7276761439.mj@gmail.com>
2018-10-25 09:38:16 +0530

    OK
    OK

---
+++ SpringDataJPA_CRUD_Repository/src/main/java/com/client/Test.java
@@ -0,0 +1,20 @@
+package com.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = "com")
+@EntityScan("com")
+@EnableJpaRepositories("com")
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpringApplication.run(Test.class, args);
+
+	}
+
+}
